909bd7f7875410ea7675d50e3eac95857e9c1073
Lionel E. Ramos <ltrainpr@gmail.com>
2014-03-22 23:29:02 -0600

    First test on Mars Rover Spec file

--- Mars Rover/spec/MarsRoverSpec.js
+++ Mars Rover/spec/MarsRoverSpec.js
@@ -1,5 +1,9 @@
-describe("Hello world", function() {
-    it("says hello", function() {
-        expect(helloWorld()).toEqual("Hello world!");
+var marsRover = function() {
+	return 0;
+};
+
+describe("MarsRover Kata Tests", function() {
+    it("has function named marsRover", function() {
+        expect(marsRover()).toBe(0);
     });
 });
