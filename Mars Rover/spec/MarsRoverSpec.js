101b56517932607a5909d157c6c75dfede5f8645
Lionel E. Ramos <ltrainpr@gmail.com>
2014-03-23 19:39:08 -0600

    Mars Rover-Third Test-TDD Jasmine

--- Mars Rover/spec/MarsRoverSpec.js
+++ Mars Rover/spec/MarsRoverSpec.js
@@ -1,7 +1,38 @@
-
-
 describe("MarsRover Kata Tests", function() {
-    it("has function named marsRover", function() {
-        expect(marsRover()).toBeDefined();
-    });
+  var command;
+  var move = function (startingCoordinates, command) {
+		return marsRover.move(startingCoordinates, command);
+  };
+
+  var variables = function (xAxis, yAxis, direction) {
+		return [xAxis, yAxis, direction.toLowerCase()];
+  };
+
+  it("moves rover facing north one coordinate forward", function(){
+		var startingPosition = variables(0, 0, 'N');
+		command = ['f'];
+		
+		expect(move(startingPosition, command)).toEqual([0,1,'N']);
+  });
+
+  it("moves rover facing north to face East", function(){
+		var startingPosition = variables(0, 0, 'N');
+		command = ['r'];
+		
+		expect(move(startingPosition, command)).toEqual([0,0,'E']);
+  });
+
+  it("moves rover facing north to face West", function(){
+		var startingPosition = variables(0, 0, 'N');
+		command = ['l'];
+		
+		expect(move(startingPosition, command)).toEqual([0,0,'W']);
+  });
+
+  it("turns rover left and moves forward one", function(){
+		var startingPosition = variables(0, 0, 'N');
+		command = ['l', 'f'];
+		
+		expect(move(startingPosition, command)).toEqual([-1,0,'W']);
+  });
 });
--- Mars Rover/src/MarsRover.js
+++ Mars Rover/src/MarsRover.js
@@ -1,4 +1,55 @@
+var marsRover = {
 
-	var marsRover = function(position, commands) {
-		return 0;
-	};
+	move: function (startingCoordinates, command) {
+		var x, y;
+		for(var i=0; i < command.length; i++){
+
+			if (command[i] == 'f'){
+				y = startingCoordinates[1] + 1;
+				x = startingCoordinates[0];
+			}else if(command[i] == 'b'){
+				y = startingCoordinates[1] - 1;
+				x = startingCoordinates[0];
+			}else if(command[i] == 'l'){
+				y = startingCoordinates[1];
+				x = startingCoordinates[0];
+				startingCoordinates[2] = marsRover.left(startingCoordinates[2]);
+			}else if (command[i] == 'r'){
+				y = startingCoordinates[1];
+				x = startingCoordinates[0];
+				startingCoordinates[2] = marsRover.right(startingCoordinates[2]);
+			}
+			return [x,y,startingCoordinates[2].toUpperCase()];
+		}
+	},
+
+	right: function (currentDirection) {
+		switch(currentDirection){
+			case 'n':
+				return 'e';
+			case 'e':
+				return 's';
+			case 's':
+				return 'w';
+			case 'w':
+				return 'n';
+			default:
+				alert('default');
+		}
+	},
+
+	left: function (currentDirection) {
+		switch(currentDirection){
+			case 'n':
+				return 'w';
+			case 'e':
+				return 'n';
+			case 's':
+				return 'e';
+			case 'w':
+				return 's';
+			default:
+				alert('default');
+		}
+	}
+};
