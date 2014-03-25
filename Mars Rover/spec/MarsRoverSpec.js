3a87dfe0f2f51c1529d498159bd36e35808b84a2
Lionel E. Ramos <ltrainpr@gmail.com>
2014-03-24 22:04:06 -0600

    Adds Specs & Code for fixed grid

--- Mars Rover/spec/MarsRoverSpec.js
+++ Mars Rover/spec/MarsRoverSpec.js
@@ -1,35 +1,54 @@
 describe("MarsRover Kata Tests", function() {
-  var command;
-  var move = function (startingCoordinates, command) {
-		return marsRover.move(startingCoordinates, command);
+  var command, grid, startingPosition;
+  var move = function (startingCoordinates, command, grid) {
+		return marsRover.move(startingCoordinates, command, grid);
   };
-  
 
   it("moves rover facing north one coordinate forward", function(){
-		var startingPosition = [0, 0, 'N'];
+		grid = [30, 30];
+		startingPosition = [0, 0, 'N'];
 		command = ['f'];
 		
-		expect(move(startingPosition, command)).toEqual([0,1,'N']);
+		expect(move(startingPosition, command, grid)).toEqual([0,1,'N']);
   });
 
   it("moves rover facing north to face East", function(){
-		var startingPosition = [0, 0, 'N'];
+		grid = [30, 30];
+		startingPosition = [0, 0, 'N'];
 		command = ['r'];
 		
-		expect(move(startingPosition, command)).toEqual([0,0,'E']);
+		expect(move(startingPosition, command, grid)).toEqual([0,0,'E']);
   });
 
   it("moves rover facing north to face East", function(){
-		var startingPosition = [0, 0, 'N'];
+		grid = [30, 30];
+		startingPosition = [0, 0, 'N'];
 		command = ['r', 'f'];
 		
-		expect(move(startingPosition, command)).toEqual([1,0,'E']);
+		expect(move(startingPosition, command, grid)).toEqual([1,0,'E']);
   });
 
   it("turns rover right twice and moves forward twice and backwards once", function(){
-		var startingPosition = [5, 6, 'E'];
+		grid = [30, 30];
+		startingPosition = [5, 6, 'E'];
 		command = ['r', 'r', 'f', 'f', 'b'];
 		
-		expect(move(startingPosition, command)).toEqual([4,6,'W']);
+		expect(move(startingPosition, command, grid)).toEqual([4,6,'W']);
+  });
+
+  it("wrapping of X axis grid", function(){
+		grid = [30, 30];
+		startingPosition = [29, 0, 'E'];
+		command = ['f'];
+
+		expect(move(startingPosition, command, grid)).toEqual([0, 0, 'E']);
+  });
+
+  it("wrapping of Y axis grid", function(){
+		grid = [30, 30];
+		startingPosition = [5, 29, 'N'];
+		command = ['f'];
+
+		expect(move(startingPosition, command, grid)).toEqual([5, 0, 'N']);
   });
 });
--- Mars Rover/src/MarsRover.js
+++ Mars Rover/src/MarsRover.js
@@ -1,6 +1,6 @@
 var marsRover = {
-	move: function (startingCoordinates, command) {
-		var x, y, newDirection;
+	move: function (startingCoordinates, command, grid) {
+		var x, y, newDirection, maxHorizontal, maxVertical;
 		function xyCoordinates (startingCoordinates) {
 			x = startingCoordinates[0];
 			y = startingCoordinates[1];
@@ -15,19 +15,19 @@ var marsRover = {
 			}else if (command[i] == 'f'){
 				switch(startingCoordinates[2].toLowerCase()){
 					case 'n':
-						marsRover.north(startingCoordinates, command[i]);
+						marsRover.north(startingCoordinates, command[i], grid);
 						xyCoordinates(startingCoordinates);
 						break;
 					case 'e':
-						marsRover.east(startingCoordinates, command[i]);
+						marsRover.east(startingCoordinates, command[i], grid);
 						xyCoordinates(startingCoordinates);
 						break;
 					case 's':
-						marsRover.south(startingCoordinates, command[i]);
+						marsRover.south(startingCoordinates, command[i], grid);
 						xyCoordinates(startingCoordinates);
 						break;
 					case 'w':
-						marsRover.west(startingCoordinates, command[i]);
+						marsRover.west(startingCoordinates, command[i], grid);
 						xyCoordinates(startingCoordinates);
 						break;
 					default:
@@ -36,19 +36,19 @@ var marsRover = {
 			}else if (command[i] == 'b'){
 				switch(startingCoordinates[2].toLowerCase()){
 					case 'n':
-						marsRover.north(startingCoordinates, command[i]);
+						marsRover.north(startingCoordinates, command[i], grid);
 						xyCoordinates(startingCoordinates);
 						break;
 					case 'e':
-						marsRover.east(startingCoordinates, command[i]);
+						marsRover.east(startingCoordinates, command[i], grid);
 						xyCoordinates(startingCoordinates);
 						break;
 					case 's':
-						marsRover.south(startingCoordinates, command[i]);
+						marsRover.south(startingCoordinates, command[i], grid);
 						xyCoordinates(startingCoordinates);
 						break;
 					case 'w':
-						marsRover.west(startingCoordinates, command[i]);
+						marsRover.west(startingCoordinates, command[i], grid);
 						xyCoordinates(startingCoordinates);
 						break;
 					default:
@@ -60,52 +60,76 @@ var marsRover = {
 		return [x,y,startingCoordinates[2].toUpperCase()];
 	},
 
-	north: function (startingCoordinates, singleCommand) {
+	north: function (startingCoordinates, singleCommand, grid) {
 		if (singleCommand == 'f'){
-			startingCoordinates[1] = startingCoordinates[1] + 1;
-			startingCoordinates[0] = startingCoordinates[0];
+			if((startingCoordinates[1] + 1) === grid[1]){
+				startingCoordinates[1] = 0;
+			}else{
+				startingCoordinates[1] += 1;
+			}
 		}else if(singleCommand == 'b'){
-			startingCoordinates[1] = startingCoordinates[1] - 1;
-			startingCoordinates[0] = startingCoordinates[0];
+			if((startingCoordinates[1] - 1) === 0){
+				startingCoordinates[1] = grid[1] - 1;
+			}else{
+				startingCoordinates[1] -= 1;
+			}
 		}else{
 			alert('North: Not supposed to get here');
 		}
 		return startingCoordinates;
 	},
 
-	east: function (startingCoordinates, singleCommand) {
+	east: function (startingCoordinates, singleCommand, grid) {
 		if (singleCommand == 'f'){
-			startingCoordinates[1] = startingCoordinates[1];
-			startingCoordinates[0] = startingCoordinates[0] + 1;
+			if((startingCoordinates[0] + 1) === grid[0]){
+				startingCoordinates[0] = 0;
+			}else{
+				startingCoordinates[0] += 1;
+			}
 		}else if(singleCommand == 'b'){
-			startingCoordinates[1] = startingCoordinates[1];
-			startingCoordinates[0] = startingCoordinates[0] + 1;
+			if(startingCoordinates[0] === 0){
+				startingCoordinates[0] = grid[0] - 1;
+			}else{
+				startingCoordinates[0] -= 1;
+			}
 		}else{
 			alert('North: Not supposed to get here');
 		}
 		return startingCoordinates;
 	},
 
-	south: function (startingCoordinates, singleCommand) {
+	south: function (startingCoordinates, singleCommand, grid) {
 		if (singleCommand == 'f'){
-			startingCoordinates[1] = startingCoordinates[1] - 1;
-			startingCoordinates[0] = startingCoordinates[0];
+			if(startingCoordinates[1] === 0){
+				startingCoordinates[1] = grid[1] -1;
+			}else{
+				startingCoordinates[1] -= 1;
+			}
 		}else if(singleCommand == 'b'){
-			startingCoordinates[1] = startingCoordinates[1] + 1;
-			startingCoordinates[0] = startingCoordinates[0];
+			if((startingCoordinates[1] + 1) === grid[1]){
+				startingCoordinates[1] = 0;
+			}else{
+				startingCoordinates[1] += 1;
+			}
 		}else{
 			alert('North: Not supposed to get here');
 		}
 		return startingCoordinates;
 	},
 
-	west: function (startingCoordinates, singleCommand) {
+	west: function (startingCoordinates, singleCommand, grid) {
 		if (singleCommand == 'f'){
-			startingCoordinates[1] = startingCoordinates[1];
-			startingCoordinates[0] = startingCoordinates[0] - 1;
+			if(startingCoordinates[0] === 0){
+				startingCoordinates[0] = grid[0] - 1;
+			}else{
+				startingCoordinates[0] -= 1;
+			}
 		}else if(singleCommand == 'b'){
-			startingCoordinates[1] = startingCoordinates[1];
-			startingCoordinates[0] = startingCoordinates[0] + 1;
+			if((startingCoordinates[0] + 1) === grid[0]){
+				startingCoordinates[0] = 0;
+			}else{
+				startingCoordinates[0] += 1;
+			}
 		}else{
 			alert('North: Not supposed to get here');
 		}
