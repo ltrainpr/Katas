3f146386b1dd4aed48b14a9dfa38c6c5a86d80ba
Lionel E. Ramos <ltrainpr@gmail.com>
2014-05-14 22:39:14 -0600

    Adds test for unrecognized command

--- Mars Rover/spec/MarsRoverSpec.js
+++ Mars Rover/spec/MarsRoverSpec.js
@@ -71,4 +71,11 @@ describe("MarsRover Kata Tests", function() {
 
     expect(move(startingPosition, command, grid, obstacle)).toEqual([4, 4, 'E']);
   });
+
+  it("unrecognized command", function(){
+    command = ['l', 'b', 'j'];
+    obstacle = [3, 4];
+
+    expect(move(startingPosition, command, grid, obstacle)).toBeUndefined();
+  });
 });
